79f15ab0b9a55a8af70d5bc4b2d1c3ff8fc457a4
Albin Remnestål <albin.remnestal@gmail.com>
2018-03-25 17:32:56 +0200

    feat: transition matix for pairwise probability

--- fakenews.py
+++ fakenews.py
@@ -3,7 +3,26 @@ from os.path import isfile, join
 from os import listdir
 
 def main():
-    print(_frequency_matrix())
+    print(_transition_matrix())
+
+def _transition_matrix():
+    """ Return a unidirectional transition matrix of pairwise word order
+        * frequency is relative to all successors of a given word
+        * matrix elements are represented by tuples of (word, probability)
+    """
+    # the transition matrix is implemented as a dictionary of tuple-entry lists
+    transition = defaultdict(list)
+    for word, successors in _frequency_matrix().items():
+        total_freq = sum(map(lambda d: d[1], successors.items()))
+
+        for successor, frequency in successors.items():
+            # matrix entries consist of a word and the probability of transition
+            probability = float(frequency) / float(total_freq)
+            transition[word].append((successor, probability))
+
+        # sort the successor list by probability for simpler use later
+        transition[word].sort(key=lambda entry: entry[1])
+    return transition
 
 def _frequency_matrix():
     """ Return a unidirectional frequency matrix of pairwise word order

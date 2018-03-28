fe6cc8f16106d15ec74e0a4d990ac849c06bdf94
Albin Remnestål <albin.remnestal@gmail.com>
2018-03-28 19:35:35 +0200

    feat: methods for adding words to the matrix

--- matrix.py
+++ matrix.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 
 class frequency(object):
     """ Unidirectional matrix of ascending pairwise word frequency
@@ -8,3 +9,19 @@ class frequency(object):
     def __init__(self):
         """ Initialize an empty frequency matrix """
         self.frequency = defaultdict(lambda: defaultdict(int))
+
+    def add_text(self, text):
+        """ Add each pair of words in the passed text to the matrix """
+        words = text.split()
+        words_pairwise = zip(words, words[1:]+[None])
+        for pair in enumerate(words_pairwise):
+            self.add_pair(*pair)
+
+    def add_pair(self, word, succesor):
+        """ Add the passed pair of words to the matrix
+
+            The second argument is added as the successor of the first word.
+            Since the frequency is unidirectional, the first argument is not
+            added as the successor of the second.
+        """
+        self.frequency[word][succesor] += 1

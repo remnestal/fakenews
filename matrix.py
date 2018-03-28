1733f4d302294a2b5107907cb3b9aa4a93da7834
Albin Remnestål <albin.remnestal@gmail.com>
2018-03-28 19:59:52 +0200

    feat: transition probability matrix

--- matrix.py
+++ matrix.py
@@ -28,3 +28,23 @@ class frequency(object):
             added as the successor of the second.
         """
         self.frequency[word][succesor] += 1
+
+class transition(object):
+    """ Word sequence transition matrix
+
+        Matrix containing the transition probability of each word. For each word
+        there is a list of the tuple (next-word, probability) and the words are
+        sorted in ascending order. `None` is used as an end-of-line delimiter.
+    """
+    def __init__(self, frequencies):
+        self.transition = defaultdict(list)
+        for (word, nextlist) in frequencies.frequency.items():
+            frequency_sum = sum((freq for _, freq in nextlist.items()))
+
+            for (next_word, frequency) in nextlist.items():
+                # matrix entries consist of a word and the probability of transition
+                probability = float(frequency) / float(total_freq)
+                transition[word].append((next_word, probability))
+
+            # sort the next-word list by probability for simpler use later
+            transition[word].sort(key=lambda entry: entry[1])

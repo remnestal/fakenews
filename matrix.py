2dd8d7f7f70b1ffa57de0a61240c5db5f55f3a10
Albin Remnestål <albin.remnestal@gmail.com>
2018-03-29 15:02:21 +0200

    feat: add positional dimension to frequency matrix

--- matrix.py
+++ matrix.py
@@ -34,23 +34,23 @@ class frequency(object):
 
     def __init__(self):
         """ Initialize an empty frequency matrix """
-        self.matrix = defaultdict(lambda: defaultdict(int))
+        self.matrix = _defaultlist(lambda: defaultdict(lambda: defaultdict(int)))
 
     def add_text(self, text):
         """ Add each pair of words in the passed text to the matrix """
         words = text.split()
         words_pairwise = zip(words, words[1:]+[None])
-        for pair in words_pairwise:
-            self.add_pair(*pair)
+        for position, pair in enumerate(words_pairwise):
+            self.add_pair(position, *pair)
 
-    def add_pair(self, word, successor):
+    def add_pair(self, position, word, successor):
         """ Add the passed pair of words to the matrix
 
             The second argument is added as the successor of the first word.
             Since the frequency is unidirectional, the first argument is not
             added as the successor of the second.
         """
-        self.matrix[word][successor] += 1
+        self.matrix[position][word][successor] += 1
 
 class transition(object):
     """ Word sequence transition matrix

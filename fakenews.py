485a63302c1cc49bf8c66349948dde8793d3c4ef
Albin Remnestål <albin.remnestal@gmail.com>
2018-03-30 14:42:01 +0200

    feat: add variable amount of headlines

--- fakenews.py
+++ fakenews.py
@@ -1,9 +1,20 @@
 import markovmodel
+import sys
 
 def main():
     """ Generate fake headlines """
     chain = markovmodel.Markovchain()
-    print(chain.generate())
+    num_samples = 10
+
+    # let user define number of generated samples
+    if len(sys.argv) > 1:
+        try:
+            num_samples = int(sys.argv[1])
+        except:
+            sys.exit('1st argument must be an integer')
+
+    for _ in range(num_samples):
+        print(chain.generate())
 
 if __name__ == "__main__":
     main()

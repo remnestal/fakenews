74b3e35f4281e64108d5c2b683741f95f124c13e
Albin Remnestål <albin.remnestal@gmail.com>
2018-04-20 21:34:57 +0200

    feat: add class for 3D lazily evaluated matrix

---
+++ matrix3.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+
+class _3d_matrix(object):
+    """ Basic implementation for a 3-dimensional lazily evaluated matrix
+
+        The matrix has no fixed size and is dynamically allocated using
+        collections.defaultdict to achieve JIT construction of missing elements.
+        This means that, for example, element A[1][2][3] can be assigned a value
+        even if neither element A[1][2] or element A[1] has been defined before,
+        since both will be instantiated on the fly.
+    """
+
+    def __init__(self, matrix_type):
+        """ Initialize an empty matrix """
+        # assert that the passed type is actually derived from the type-class
+        if type(matrix_type) is not type:
+             raise ValueError('`matrix_type` must be derived of class `type`')
+        else:
+            # This is necessary evil to achieve lazy evaluation
+            self.__matrix = defaultdict(lambda: defaultdict(lambda: defaultdict(matrix_type)))
